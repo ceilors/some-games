ab5051a242cca156a6f982eb53b13ef7ecda4e76
Alexey Golubev <dr.freecx@gmail.com>
2018-06-24 20:13:32 +0300

    game code update
    - add drag and drop
    - add basket for figures
    - other staff

--- 1010.py
+++ 1010.py
@@ -2,7 +2,6 @@
 import pygame
 import numpy as np
 
-
 colors = {
     0: (200, 200, 200),
     1: (255, 206, 55),
@@ -16,17 +15,22 @@ colors = {
     9: (62, 232, 210),
 }
 
-figures = [
-    [
-        [1, 1],
-        [1, 0],
-    ],
-    [[1, 0], [1, 1]],
-]
+game_width = 500
+game_height = 500
+
+figures = [np.array([[1, 1], [1, 1]]), np.array([[1, 1], [0, 1]]), np.array([[1] * 5])]
+figures = np.array([figure.T for figure in figures])
 
 shift_pos = (10, 10)
 TILE_SIZE = 32
 TILE_SHIFT = 28
+
+# mouse pressed key
+LEFT_MOUSE = 1
+RIGHT_MOUSE = 3
+
+# координаты с корзинам с фигурами
+basket_pos = [[x, game_height - game_height // 3] for x in np.arange(0, game_width, game_width / 3)]
 
 
 def AAfilledRoundedRect(surface, rect, color, radius=0.2):
@@ -65,11 +69,20 @@ class Board:
         self.cells = np.zeros((size_y, size_y))
 
     def set(self, pos, figure, color=1):
-        width, height = len(figure[0]), len(figure)
+        width, height = len(figure), len(figure[0])
         x_pos, y_pos = pos[0], pos[1]
         for y in range(y_pos, y_pos + height):
             for x in range(x_pos, x_pos + width):
-                self.cells[y][x] = color * figure[y - y_pos][x - x_pos]
+                self.cells[y, x] = color * figure[x - x_pos][y - y_pos]
+
+    def can_set(self, pos, figure):
+        width, height = len(figure), len(figure[0])
+        x_pos, y_pos = pos[0], pos[1]
+        for y in range(y_pos, y_pos + height):
+            for x in range(x_pos, x_pos + width):
+                if self.cells[y, x] != 0:
+                    return False
+        return True
 
     def draw(self, display):
         for j, row in enumerate(self.cells):
@@ -78,20 +91,26 @@ class Board:
                 AAfilledRoundedRect(display, rect, colors[item])
 
     def get_lines(self):
+        items = []
         for row in self.cells:
             if np.all(row > 0):
-                yield row
+                items.append(row)
         for col in self.cells.T:
-            if np.all(row > 0):
-                yield row
+            if np.all(col > 0):
+                items.append(col)
+        return items
+
 
 board = Board()
-board.set((1, 1), figures[0], color=9)
-board.set((2, 2), figures[1], color=8)
 
 
 class App:
-    def __init__(self, x=100, y=100, width=640, height=480):
+    def __init__(self, x=100, y=100, width=game_width, height=game_height):
+        self.figure_drag = False
+        self.figure_index = -1
+        self.basket_count = 0
+        self.basket_figures = np.random.choice(figures, size=3)
+        self.basket_colors = np.random.randint(1, 9 + 1, size=3)
         self.width = width
         self.height = height
         self.x, self.y = x, y
@@ -106,13 +125,49 @@ class App:
     def on_event(self, event):
         if event.type == pygame.QUIT:
             self._running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == LEFT_MOUSE:
+                x, y = pygame.mouse.get_pos()
+                if not self.figure_drag and y >= basket_pos[0][1]:
+                    self.figure_index = x // (game_width // 3)
+                    self.figure_drag = True
+        if event.type == pygame.MOUSEBUTTONUP:
+            if event.button == LEFT_MOUSE:
+                x, y = pygame.mouse.get_pos()
+                figure_in = x > shift_pos[0] and x < (shift_pos[0] + TILE_SIZE * 10) and\
+                    y > shift_pos[1] and y < (shift_pos[1] + TILE_SIZE * 10)
+                if figure_in:
+                    index_x = int(np.ceil((x - shift_pos[0] + 8) / TILE_SIZE) - 1)
+                    index_y = int(np.ceil((y - shift_pos[1] + 8) / TILE_SIZE) - 1)
+                    if board.can_set((index_x, index_y), self.basket_figures[self.figure_index]):
+                        board.set(
+                            (index_x, index_y), self.basket_figures[self.figure_index], color=self.basket_colors[self.figure_index])
+                        self.basket_figures[self.figure_index] = np.array([[]])
+                        self.basket_count += 1
+                        if self.basket_count == 3:
+                            self.basket_figures = np.random.choice(figures, size=3)
+                            self.basket_colors = np.random.randint(1, 9 + 1, size=3)
+                            self.basket_count = 0
+                self.figure_drag = False
+                self.figure_index = -1
 
     def on_loop(self):
-        pass
+        for item in board.get_lines():
+            item[:] = 0
 
     def on_render(self):
         pygame.draw.rect(self._display_surf, (100, 100, 100), (0, 0, self.width, self.height))
         board.draw(self._display_surf)
+        for index, (x, y) in enumerate(basket_pos):
+            if index == self.figure_index:
+                x, y = pygame.mouse.get_pos()
+                x -= TILE_SIZE // 2
+                y -= TILE_SIZE // 2
+            for j, row in enumerate(self.basket_figures[index].T):
+                for i, item in enumerate(row):
+                    if item > 0:
+                        rect = (x + i * TILE_SIZE + 8, y + j * TILE_SIZE + 8, TILE_SHIFT, TILE_SHIFT)
+                        AAfilledRoundedRect(self._display_surf, rect, colors[self.basket_colors[index]])
         pygame.display.flip()
 
     def on_cleanup(self):
