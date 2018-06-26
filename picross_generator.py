c76cefac0780e4a93433f908a2d50ba395b5fbb6
Alexey Golubev <dr.freecx@gmail.com>
2018-06-26 12:46:22 +0300

    fix bugs and code improvement

--- picross_generator.py
+++ picross_generator.py
@@ -6,6 +6,7 @@ def by_two(arr):
     for index in range(len(arr) - 1):
         yield arr[index], arr[index + 1]
 
+
 def split_and_count(arr, indexes, ignore_zeros=True):
     results = []
     indexes = [0] + indexes + [len(arr)]
@@ -15,12 +16,14 @@ def split_and_count(arr, indexes, ignore_zeros=True):
             results.append((block[0], len(block)))
     return results
 
+
 def split_indexes(arr):
     indexes = []
     for index, (a, b) in enumerate(zip(arr[:-1], arr[1:])):
         if a != b:
             indexes.append(index + 1)
     return indexes
+
 
 def get_tuples(arr):
     data = []
@@ -29,25 +32,27 @@ def get_tuples(arr):
         data.append(split_and_count(line, indexes))
     return data
 
+
 def get_color(palette, index):
     r = palette[index * 3 + 0]
     g = palette[index * 3 + 1]
     b = palette[index * 3 + 2]
     return (r, g, b)
 
-if __name__ == '__main__':
-    BLOCK_SIZE = 32
 
-    img = Image.open('resources/mario.png')
+def generate_picross(input_image, output_image, block_size=32, render_font='resources/FiraMono-Regular.ttf'):
+    # читаем картинку и преобразуем цвета в палитру
+    img = Image.open(input_image).convert('P')
 
+    # получаем все цвета палитры
     palette = bytearray(img.palette.palette)
-    
-    # получаем цифры по картинке
+
+    # получаем последовательности цветов на картинке
     arr = np.array(img)
     horizontal_data = get_tuples(arr)
     vertical_data = get_tuples(arr.T)
 
-    # находим самый длинный блок с цифрами
+    # находим самый длинный блок
     horizontal_blocks = 0
     for item in horizontal_data:
         horizontal_blocks = max(len(item), horizontal_blocks)
@@ -56,43 +61,47 @@ if __name__ == '__main__':
         vertical_blocks = max(len(item), vertical_blocks)
 
     # расчитываем размер новой картинки
-    nimage_width = BLOCK_SIZE * img.size[0] + horizontal_blocks * BLOCK_SIZE
-    nimage_height = BLOCK_SIZE * img.size[1] + vertical_blocks * BLOCK_SIZE
+    nimage_width = block_size * img.size[0] + horizontal_blocks * block_size
+    nimage_height = block_size * img.size[1] + vertical_blocks * block_size
 
     nimage = Image.new('RGBA', (nimage_width + 1, nimage_height + 1), 'white')
     draw = ImageDraw.Draw(nimage)
 
     # ресайзим старую картинку
-    picture = img.resize((BLOCK_SIZE * img.size[0], BLOCK_SIZE * img.size[1]), Image.BILINEAR)
+    picture = img.resize((block_size * img.size[0], block_size * img.size[1]), Image.BILINEAR)
     # и переносим на новую
-    box = (horizontal_blocks * BLOCK_SIZE, vertical_blocks * BLOCK_SIZE, nimage_width, nimage_height)
+    box = (horizontal_blocks * block_size, vertical_blocks * block_size, nimage_width, nimage_height)
     nimage.paste(picture, box=box)
-    # и рисуем рамку
+    # рисуем рамку
     draw.rectangle(box, outline='black')
 
-    # рисуем линии
-    for x in range(horizontal_blocks * BLOCK_SIZE, nimage_width + 1, BLOCK_SIZE):
+    # рисуем клеточки
+    for x in range(horizontal_blocks * block_size, nimage_width + 1, block_size):
         draw.line((x, 0, x, nimage_height), 'black')
-    for x in range(0, horizontal_blocks * BLOCK_SIZE, BLOCK_SIZE):
-        draw.line((x, horizontal_blocks * BLOCK_SIZE, x, nimage_height), 'black')
-    for y in range(vertical_blocks * BLOCK_SIZE, nimage_height + 1, BLOCK_SIZE):
+    for x in range(0, horizontal_blocks * block_size, block_size):
+        draw.line((x, vertical_blocks * block_size, x, nimage_height), 'black')
+    for y in range(vertical_blocks * block_size, nimage_height + 1, block_size):
         draw.line((0, y, nimage_width, y), 'black')
-    for y in range(0, vertical_blocks * BLOCK_SIZE, BLOCK_SIZE):
-        draw.line((horizontal_blocks * BLOCK_SIZE, y, nimage_width, y), 'black')
+    for y in range(0, vertical_blocks * block_size, block_size):
+        draw.line((horizontal_blocks * block_size, y, nimage_width, y), 'black')
 
-    # растановляем цифры
-    font = ImageFont.truetype('resources/FiraMono-Regular.ttf', size=BLOCK_SIZE - 10)
+    # растановляем числовые значения для цветов
+    font = ImageFont.truetype(render_font, size=block_size - 10)
     for xindex, line in enumerate(vertical_data):
-        ypos = (vertical_blocks - len(line) + 0.1) * BLOCK_SIZE
-        xpos = BLOCK_SIZE * (horizontal_blocks + 0.3 + xindex)
-        for color, number in line: 
+        ypos = (vertical_blocks - len(line) + 0.1) * block_size
+        xpos = block_size * (horizontal_blocks + 0.3 + xindex)
+        for color, number in line:
             draw.text((xpos, ypos), str(number), font=font, fill=get_color(palette, color))
-            ypos += BLOCK_SIZE
+            ypos += block_size
     for yindex, line in enumerate(horizontal_data):
-        xpos = (horizontal_blocks - len(line) + 0.3) * BLOCK_SIZE
-        ypos = BLOCK_SIZE * (vertical_blocks + yindex + 0.1)
-        for color, number in line: 
+        xpos = (horizontal_blocks - len(line) + 0.3) * block_size
+        ypos = block_size * (vertical_blocks + yindex + 0.1)
+        for color, number in line:
             draw.text((xpos, ypos), str(number), font=font, fill=get_color(palette, color))
-            xpos += BLOCK_SIZE
+            xpos += block_size
 
-    nimage.save('resources/mario-picross.png')
+    nimage.convert('P').save(output_image)
+
+
+if __name__ == '__main__':
+    generate_picross('resources/mario.png', 'resources/mario-picross.png')
