a46b584785b9bd0db4c6ac122c9f99f920789fe7
Alexey Golubev <dr.freecx@gmail.com>
2018-09-04 11:58:18 +0300

    fix & upd
    - all tiles in one line
    - level index must be string
    - add player state

--- sokoban.py
+++ sokoban.py
@@ -8,7 +8,7 @@ import os
 background_color = (100, 100, 100)
 pole_size = (8, 8)
 tile_size = 32
-level = 1
+level = '1'
 
 game_clock = 30
 quit_flag = False
@@ -19,24 +19,21 @@ LEFT_MOUSE = 1
 RIGHT_MOUSE = 3
 
 # tile indexes
-TI_FIRST_LINE = 0
 TI_BACKGROUND = 0
 TI_WALL = 1
 TI_FLOOR = 2
 TI_BOX = 3
 TI_TARGET = 4
-# ...
-TI_SECOND_LINE = 1
-TI_PLAYER_RIGHT = 0
-TI_PLAYER_UP = 1
-TI_PLAYER_LEFT = 2
-TI_PLAYER_DOWN = 3
+TI_PLAYER_RIGHT = 5
+TI_PLAYER_UP = 6
+TI_PLAYER_LEFT = 7
+TI_PLAYER_DOWN = 8
 
 CWD = Path(os.path.dirname(os.path.abspath(__file__)))
 
 
-def draw_tile(img, xp, yp, xi, yi, size=tile_size):
-    tile_rect = (size * xi, size * yi, size, size)
+def draw_tile(img, xp, yp, index, size=tile_size):
+    tile_rect = (size * index, 0, size, size)
     surface.blit(img, (x * size, yp * size), tile_rect)
 
 
@@ -53,7 +50,7 @@ if __name__ == '__main__':
     game = js.load(open(CWD / 'resources' / 'sokoban_levels.json'))
 
     field = game[level]['field']
-    player = game[level]['player']
+    player = [*game[level]['player'], TI_PLAYER_UP]
     boxes = game[level]['boxes']
     targets = game[level]['targets']
 
@@ -63,17 +60,17 @@ if __name__ == '__main__':
         # field
         for y, line in enumerate(field):
             for x, value in enumerate(line):
-                draw_tile(tiles, x, y, value, TI_FIRST_LINE)
+                draw_tile(tiles, x, y, value)
         # boxes
         for x, y in boxes:
-            draw_tile(tiles, x, y, TI_BOX, TI_FIRST_LINE)
+            draw_tile(tiles, x, y, TI_BOX)
 
         # targets
         for x, y in targets:
-            draw_tile(tiles, x, y, TI_TARGET, TI_FIRST_LINE)
+            draw_tile(tiles, x, y, TI_TARGET)
 
         # player
-        draw_tile(tiles, player[0], player[1], TI_PLAYER_UP, TI_SECOND_LINE)
+        draw_tile(tiles, *player)
 
         # render
         screen.blit(surface, (0, 0))
@@ -85,5 +82,13 @@ if __name__ == '__main__':
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     quit_flag = True
+                if event.key == pygame.K_UP:
+                    player[2] = TI_PLAYER_UP
+                if event.key == pygame.K_DOWN:
+                    player[2] = TI_PLAYER_DOWN
+                if event.key == pygame.K_LEFT:
+                    player[2] = TI_PLAYER_LEFT
+                if event.key == pygame.K_RIGHT:
+                    player[2] = TI_PLAYER_RIGHT
 
         clock.tick(game_clock)
